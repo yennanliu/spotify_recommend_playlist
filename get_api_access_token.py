1e44a11cb205b3596646607244433509be851e50
yennanliue <f339339@gmail.com>
2018-08-17 02:11:39 +0100

    add python script grab dynamic api access token

---
+++ get_api_access_token.py
@@ -0,0 +1,90 @@
+import json
+from flask import Flask, request, redirect, g, render_template
+import requests
+import base64
+import urllib
+import os
+
+
+"""
+
+modify from 
+https://github.com/drshrey/spotify-flask-auth-example
+
+"""
+
+
+# Authentication Steps, paramaters, and responses are defined at https://developer.spotify.com/web-api/authorization-guide/
+# Visit this url to see all the steps, parameters, and expected response. 
+
+
+
+app = Flask(__name__)
+
+#  Client Keys
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET'] 
+
+# Spotify URLS
+SPOTIFY_AUTH_URL = "https://accounts.spotify.com/authorize"
+SPOTIFY_TOKEN_URL = "https://accounts.spotify.com/api/token"
+SPOTIFY_API_BASE_URL = "https://api.spotify.com"
+API_VERSION = "v1"
+SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
+
+
+# Server-side Parameters
+CLIENT_SIDE_URL = "http://127.0.0.1"
+PORT = 8080
+REDIRECT_URI = "{}:{}/callback/q".format(CLIENT_SIDE_URL, PORT)
+SCOPE = "playlist-modify-public playlist-modify-private"
+STATE = ""
+SHOW_DIALOG_bool = True
+SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
+
+
+auth_query_parameters = {
+    "response_type": "code",
+    "redirect_uri": REDIRECT_URI,
+    "scope": SCOPE,
+    # "state": STATE,
+    # "show_dialog": SHOW_DIALOG_str,
+    "client_id": CLIENT_ID
+}
+
+@app.route("/")
+def index():
+    # Auth Step 1: Authorization
+    url_args = "&".join(["{}={}".format(key,urllib.quote(val)) for key,val in auth_query_parameters.iteritems()])
+    auth_url = "{}/?{}".format(SPOTIFY_AUTH_URL, url_args)
+    return redirect(auth_url)
+
+
+@app.route("/callback/q")
+def callback():
+    # Auth Step 4: Requests refresh and access tokens
+    auth_token = request.args['code']
+    code_payload = {
+        "grant_type": "authorization_code",
+        "code": str(auth_token),
+        "redirect_uri": REDIRECT_URI
+    }
+    base64encoded = base64.b64encode("{}:{}".format(CLIENT_ID, CLIENT_SECRET))
+    headers = {"Authorization": "Basic {}".format(base64encoded)}
+    post_request = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers)
+
+    # Auth Step 5: Tokens are Returned to Application
+    response_data = json.loads(post_request.text)
+    access_token = response_data["access_token"]
+    print (' ----- access_token : -----', response_data["access_token"])
+    #return response_data["access_token"]
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=7777)
+
+
+
