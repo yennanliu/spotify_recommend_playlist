c53d4a1f777ffc271abae13f948bbbaee24a1aee
yennanliue <f339339@gmail.com>
2018-08-17 01:27:25 +0100

    transform scrape json to dataframe

--- query_spotify_api.py
+++ query_spotify_api.py
@@ -11,9 +11,25 @@ import pandas as pd
 
 def get_recommend_spotify_api():
 	""" will get spotify API access token dynamically later """
-	scrape_data=requests.get("https://api.spotify.com/v1/recommendations?market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&min_energy=0.4&min_popularity=50", headers={"Authorization": "Bearer BQCbp7cnqQKQ_w1FU2C55plNxF3Dipf7vTzc1FH0A-o2jEUSZRxTNAG7cLX4AhX9bMAnRoWiheysYZGMDcYbBK9qx7Fu7UWl-RyNyLBT3U7-Sl4UAM6x3BhS88NjZivRy6JuMEswZkqTQ-wfIlR8mel1Ogdb2WQscQ"})
+	scrape_data=requests.get("https://api.spotify.com/v1/recommendations?market=US&seed_artists=4NHQUGzhtTLFvgF5SZesLK&seed_tracks=0c6xIDDpzE81m2q797ordA&min_energy=0.4&min_popularity=50", headers={"Authorization": "Bearer BQDA5HZO1Szrt4OR7alNZ-KzeGb_wuI5VGDBicL8kbwqj9WPQ9OJzF7aNDCy20_wBr-LOBhrrmt3qFlVOiGx0oVHAJSjPCZGfKEbxuykRSOi4FpxdMw0Qh0USyAkRQp0RepfsExJUAigGoxmH_YaCdddyFygSsbzwQ"})
 	scrape_json = scrape_data.json()
 	print ('scrape_json  : ' , scrape_json)
+	# transform json to dataframe 
+	name=[]
+	external_urls=[]
+	id_=[]
+	album_type=[]
+	for i in range(len(scrape_json['tracks'])):
+		#print (scrape_json['tracks'][i]['album'])
+		name.append(scrape_json['tracks'][i]['album']['artists'][0]['name'])
+		external_urls.append(scrape_json['tracks'][i]['album']['artists'][0]['external_urls']['spotify'])
+		id_.append(scrape_json['tracks'][i]['album']['artists'][0]['id'])
+		album_type.append(scrape_json['tracks'][i]['album']['type'])
+	data=pd.DataFrame({ 'name' : name,
+						'external_urls' : external_urls,
+						'id' : id_,
+						'album_type' : album_type})
+	print (data)
 
 
 
@@ -30,4 +46,3 @@ if __name__ == '__main__':
 
 
 
-	
