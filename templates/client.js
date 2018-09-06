68f579cdd761be7f6ad0c2f62eba3351c7a8bd1c
yennanliue <f339339@gmail.com>
2018-09-06 06:49:36 +0100

    backup song genre

--- templates/client.js
+++ templates/client.js
@@ -72,7 +72,8 @@ $(function() {
 });
 
 // get song Genres
-function getGenresList() {
+/*
+ function getGenresList() {
   $('#genres-list').empty();
   $.get('/genres?token=' + _token, function(genres) {
     genres.forEach(function(genre) {
@@ -80,6 +81,156 @@ function getGenresList() {
       $('#genres-list').append(genreButtonElement);
     });
   });
+  
+  $('#genres-list').on('change', 'input', function() {
+    if($('#genres-list input:checked').length > 5) {
+      $(this).parent().removeClass("active");
+      this.checked = false;
+      genreLimitAlert("on");
+    }
+    else {
+      genreLimitAlert("off");
+    }
+  });
+}; 
+
+/*
+
+
+function getGenresList() {
+  $('#genres-list').empty();
+    genres = [
+    "acoustic",
+    "afrobeat",
+    "alt-rock",
+    "alternative",
+    "ambient",
+    "anime",
+    "black-metal",
+    "bluegrass",
+    "blues",
+    "bossanova",
+    "brazil",
+    "breakbeat",
+    "british",
+    "cantopop",
+    "chicago-house",
+    "children",
+    "chill",
+    "classical",
+    "club",
+    "comedy",
+    "country",
+    "dance",
+    "dancehall",
+    "death-metal",
+    "deep-house",
+    "detroit-techno",
+    "disco",
+    "disney",
+    "drum-and-bass",
+    "dub",
+    "dubstep",
+    "edm",
+    "electro",
+    "electronic",
+    "emo",
+    "folk",
+    "forro",
+    "french",
+    "funk",
+    "garage",
+    "german",
+    "gospel",
+    "goth",
+    "grindcore",
+    "groove",
+    "grunge",
+    "guitar",
+    "happy",
+    "hard-rock",
+    "hardcore",
+    "hardstyle",
+    "heavy-metal",
+    "hip-hop",
+    "holidays",
+    "honky-tonk",
+    "house",
+    "idm",
+    "indian",
+    "indie",
+    "indie-pop",
+    "industrial",
+    "iranian",
+    "j-dance",
+    "j-idol",
+    "j-pop",
+    "j-rock",
+    "jazz",
+    "k-pop",
+    "kids",
+    "latin",
+    "latino",
+    "malay",
+    "mandopop",
+    "metal",
+    "metal-misc",
+    "metalcore",
+    "minimal-techno",
+    "movies",
+    "mpb",
+    "new-age",
+    "new-release",
+    "opera",
+    "pagode",
+    "party",
+    "philippines-opm",
+    "piano",
+    "pop",
+    "pop-film",
+    "post-dubstep",
+    "power-pop",
+    "progressive-house",
+    "psych-rock",
+    "punk",
+    "punk-rock",
+    "r-n-b",
+    "rainy-day",
+    "reggae",
+    "reggaeton",
+    "road-trip",
+    "rock",
+    "rock-n-roll",
+    "rockabilly",
+    "romance",
+    "sad",
+    "salsa",
+    "samba",
+    "sertanejo",
+    "show-tunes",
+    "singer-songwriter",
+    "ska",
+    "sleep",
+    "songwriter",
+    "soul",
+    "soundtracks",
+    "spanish",
+    "study",
+    "summer",
+    "swedish",
+    "synth-pop",
+    "tango",
+    "techno",
+    "trance",
+    "trip-hop",
+    "turkish",
+    "work-out",
+    "world-music"
+    ]
+    genres.forEach(function(genre) {
+      let genreButtonElement = '<label class="btn btn-salmon btn-sm"><input type="checkbox" value="' + genre + '">' + genre + '</label>';
+      $('#genres-list').append(genreButtonElement);
+    });
   
   $('#genres-list').on('change', 'input', function() {
     if($('#genres-list input:checked').length > 5) {
