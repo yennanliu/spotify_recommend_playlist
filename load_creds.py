99434936d2d61102a8bfab2fe0ac4f572bee525a
yennanliue <f339339@gmail.com>
2018-10-25 09:28:29 +0100

    add script help load spotify api creds

---
+++ load_creds.py
@@ -0,0 +1,19 @@
+# python 3 
+import yaml
+import os
+
+
+with open('.creds.yml') as f:
+    config = yaml.load(f)
+
+SPOTIPY_CLIENT_ID = config['spotify_api']['SPOTIPY_CLIENT_ID']
+SPOTIPY_CLIENT_SECRET = config['spotify_api']['SPOTIPY_CLIENT_SECRET'] 
+
+def main():
+	print (' SPOTIPY_CLIENT_ID = ', SPOTIPY_CLIENT_ID)
+	print (' SPOTIPY_CLIENT_SECRET = ', SPOTIPY_CLIENT_SECRET)
+	return SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
+
+
+if __name__ == '__main__':
+	main()
