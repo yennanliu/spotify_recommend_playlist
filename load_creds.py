7744a34fa4511ec675dc91c5b48d6a799cb49f1a
yennanliue <f339339@gmail.com>
2018-10-25 09:55:48 +0100

    print load outcome

--- load_creds.py
+++ load_creds.py
@@ -9,11 +9,8 @@ with open('.creds.yml') as f:
 SPOTIPY_CLIENT_ID = config['spotify_api']['SPOTIPY_CLIENT_ID']
 SPOTIPY_CLIENT_SECRET = config['spotify_api']['SPOTIPY_CLIENT_SECRET'] 
 
-def main():
+def get_spotify_client_id_secret():
 	print (' SPOTIPY_CLIENT_ID = ', SPOTIPY_CLIENT_ID)
 	print (' SPOTIPY_CLIENT_SECRET = ', SPOTIPY_CLIENT_SECRET)
 	return SPOTIPY_CLIENT_ID, SPOTIPY_CLIENT_SECRET
 
-
-if __name__ == '__main__':
-	main()
