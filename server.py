130803b6496bc53c83be08165df09e79f3f72ce5
yennanliue <f339339@gmail.com>
2018-11-04 11:42:29 +0000

    fix get artist logic for slide_recommend page (in case no input artist name)

--- server.py
+++ server.py
@@ -98,8 +98,7 @@ def new_releases():
     
     # Send request to the Spotify API
     new_releases = sp.new_releases(country=country, limit=20, offset=0)
-    print ('new_releases : ', new_releases)
-    
+    print ('new_releases : ', new_releases) 
     # Return the list of new releases
     return jsonify(new_releases)
 
@@ -111,22 +110,20 @@ def recommend():
     print (' request.args : ' , request.args)
     print (' request : ' , request)
     # Use the country from the query parameters, if provided
-    if 'artist' in request.args:
-        artist_ = request.args['artist']
-    else:
-        artist_ = 'HONNE'
-
-    artist_id = get_artist(artist_)['id']
+    try:
+        artist_=request.args['artist']
+        artist_id = get_artist(artist_)['id']
+    except:
+        artist_='HONNE'
+        artist_id = get_artist(artist_)['id']
     print ('artist_ : ', artist_)
     print ('artist_id : ', artist_id)
     
     """
-
     Send request to the Spotify API
     https://spotipy.readthedocs.io/en/latest/
     https://github.com/plamere/spotipy/blob/4c2c1d763a3653aa225c4af848409ec31286a6bf/spotipy/client.py#L797
-    recommend_ = sp.recommendations(seed_artists=None, seed_genres=None, seed_tracks=None, limit=20, country=None, **kwargs)
-    
+    recommend_ = sp.recommendations(seed_artists=None, seed_genres=None, seed_tracks=None, limit=20, country=None, **kwargs)   
     """
     recommend_ =  sp.recommendations(seed_artists = [artist_id],seed_genres=['dubstep','deep-house','edm'],country='FR',limit=100)
     #print ('recommendation : ', recommend_)  
@@ -138,10 +135,10 @@ def recommend():
     current_genre_dict = request.__dict__['args'].to_dict(flat=False)
 
     # in case user update current genre 
-    if len(fix_genre_dict(current_genre_dict)) > 0:
-        recommend_ =  sp.recommendations(seed_artists = [artist_id],seed_genres=fix_genre_dict(current_genre_dict),country='FR',limit=5)
-    else:
-        pass 
+    #if len(fix_genre_dict(current_genre_dict)) > 0:
+    #    recommend_ =  sp.recommendations(seed_artists = [artist_id],seed_genres=fix_genre_dict(current_genre_dict),country='FR',limit=5)
+    #else:
+    #    pass 
 
     print (' 1) current request_get_status  : ', jsonify(request.get_json()) )
     print (' 2) current request_get_json : ',  (request.__dict__) )
